641d2dc79c73bfc12f1f7f30c05fdbb6fd432b48
SongX <a1647478489@outlook.com>
2022-08-17 09:06:04 +0800

    调整文件结构，将文件分级存放

--- LinuxDemo/log.cpp
+++ LinuxDemo/log.cpp
@@ -1,7 +1,25 @@
+#include<string.h>
+#include<time.h>
+#include<sys/time.h>
+#include<stdarg.h>
+#include<pthread.h>
 #include "log.h"
+
+using namespace std;
+
+//构造和析构
+Log::Log()
+{
+    m_count = 0;
+    m_is_async = false;
+}
 
 Log::~Log()
 {
+    if (m_fp != NULL)
+    {
+        fclose(m_fp);
+    }
 }
 
 bool Log::init(const char* file_name, int close_log, int log_buf_size, int split_lines, int max_queue_size)
