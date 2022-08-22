67a10430cfc0e16e411a69ac0fcfd248725851d5
SongX <1647478489@qq.com>
2022-08-22 14:15:48 +0800

    添加HTTPConnection类的接口，相关函数未定义。

--- LinuxDemo/HTTP_Conn.h
+++ LinuxDemo/HTTP_Conn.h
@@ -1,5 +1,5 @@
 /*
-HTTP 连接处理类，通过主从状态机封存Http连接类。
+	HTTP 连接处理类，通过主从状态机封存Http连接类。
 	主状态机在内部调用从状态机，从状态机将处理状态和数据传给主状态机。
 */
 #ifndef HTTPCONNECTION_H
@@ -26,8 +26,131 @@ HTTP 连接处理类，通过主从状态机封存Http连接类。
 #include<sys/uio.h>
 #include<map>
 
-class HTTP_Conn
+#include"locker.h"
+#include"SQL_connectionPool.h"
+#include"lst_timer.h"
+#include"log.h"
+
+class http_conn
 {
+public:
+	static const int FILENAME_LEN = 200;
+	static const int READ_BUFFER_SIZE = 2048;
+	static const int WRITE_BUFFER_SIZE = 1024;
+	enum METHOD
+	{
+		GET = 0,
+		POST,
+		HEAD,
+		PUT,
+		DELETE,
+		TRACE,
+		OPTIONS,
+		CONNECT,
+		PATH
+	};
+	enum CHECK_STATE
+	{
+		CHECK_STATE_REQUESTLINE = 0,
+		CHECK_STATE_HEADER,
+		CHECK_STATE_CONTENT
+	};
+	enum HTTP_CODE
+	{
+		NO_REQUEST,
+		GET_REQUEST,
+		BAD_REQUEST,
+		NO_RESOURCE,
+		FORBIDDEN_REQUEST,
+		FILE_REQUEST,
+		INTERNAL_ERROR,
+		CLOSED_CONNECTION
+	};
+	enum LINE_STATUS
+	{
+		HTTP_OK = 0,
+		HTTP_BAD,
+		HTTP_OPEN
+	};
+
+public:
+	http_conn() {}
+	~http_conn() {}
+
+public:
+	void init(int sockfd, const sockaddr_in& addr, char*, int, int, string user, string password, string database);
+	void close_conn(bool real_close = true);
+	void process();
+	bool read_once();
+	bool write();
+	sockaddr_in* get_address()
+	{
+		return &m_address;
+	}
+	void initmysql_result(connection_pool* connPool);
+	int timer_flag;
+	int improv;
+	LINE_STATUS parse_line();
+	void unmap();
+	bool add_response(const char* format, ...);
+	bool add_content(const char* content);
+	bool add_status_line(int status, const char* title);
+	bool add_headers(int content_length);
+	bool add_content_type();
+	bool add_content_length(int content_length);
+	bool add_linger();
+	bool add_blank_line();
+
+public:
+	static int m_epollfd;
+	static int m_user_count;
+	MYSQL* mysql;
+	int m_state;	//读为0，写为1
+
+private:
+	void init();
+	HTTP_CODE process_read();
+	bool process_write(HTTP_CODE ret);
+	HTTP_CODE parse_request_line(char* text);
+	HTTP_CODE parse_headers(char* text);
+	HTTP_CODE parse_content(char* text);
+	HTTP_CODE do_request();
+	char* get_line() { return m_read_buf + m_start_line; };
+
+private:
+	int m_sockfd;
+	sockaddr_in m_address;
+	char m_read_buf[READ_BUFFER_SIZE];
+	int m_read_idx;
+	int m_checked_idx;
+	int m_start_line;
+	char m_write_buf[WRITE_BUFFER_SIZE];
+	int m_write_idx;
+	CHECK_STATE m_check_state;
+	METHOD m_method;
+	char m_real_file[FILENAME_LEN];
+	char* m_url;
+	char* m_version;
+	char* m_host;
+	int m_content_length;
+	bool m_linger;
+	char* m_file_address;
+	struct stat m_file_stat;
+	struct iovec m_iv[2];
+	int m_iv_count;
+	int cgi;
+	char* m_string;
+	int bytes_to_send;
+	int bytes_have_send;
+	char* doc_root;
+
+	map<string, string> user;
+	int m_TRIGMODE;
+	int m_close_log;
+
+	char sql_user[100];
+	char sql_password[100];
+	char sql_database[100];
 
 };
 
--- LinuxDemo/lst_timer.cpp
+++ LinuxDemo/lst_timer.cpp
@@ -1,1 +1,65 @@
 #include "lst_timer.h"
+
+sort_timer_lst::sort_timer_lst()
+{
+	
+}
+
+sort_timer_lst::~sort_timer_lst()
+{
+
+}
+
+void sort_timer_lst::add_timer(util_timer* timer)
+{
+}
+
+void sort_timer_lst::adjust_timer(util_timer* timer)
+{
+}
+
+void sort_timer_lst::del_timer(util_timer* timer)
+{
+}
+
+void sort_timer_lst::tick()
+{
+}
+
+void sort_timer_lst::add_timer(util_timer* timer, util_timer* lst_head)
+{
+}
+
+void Utils::init(int timeslot)
+{
+}
+
+int Utils::setnonblocking(int fd)
+{
+	return 0;
+}
+
+void Utils::addfd(int epollfd, int fd, bool one_shot, int TRIGMode)
+{
+}
+
+void Utils::sig_handler(int sig)
+{
+}
+
+void Utils::addsig(int sig, void(handler)(int), bool restart)
+{
+}
+
+void Utils::timer_handler()
+{
+}
+
+void Utils::show_error(int connfd, const char* info)
+{
+}
+
+class Utils;
+void cb_func(client_data* user_data)
+{
+}
