8d39d1e8ff595d02d2be3628d95461564090e25c
“SSONNGG” <1647478489@qq.com>
2022-08-22 21:50:44 +0800

    为lst_timer实现类添加信号函数的注释

--- LinuxDemo/lst_timer.cpp
+++ LinuxDemo/lst_timer.cpp
@@ -7,6 +7,7 @@ sort_timer_lst::sort_timer_lst()
 	tail = NULL;
 }
 
+//链表被销毁时，删除其中的所有定时器
 sort_timer_lst::~sort_timer_lst()
 {
     util_timer* tmp = head;
@@ -123,6 +124,7 @@ void sort_timer_lst::tick()
     }
 }
 
+
 void sort_timer_lst::add_timer(util_timer* timer, util_timer* lst_head)
 {
     util_timer* prev = lst_head;
@@ -186,27 +188,43 @@ void Utils::addfd(int epollfd, int fd, bool one_shot, int TRIGMode)
     setnonblocking(fd);
 }
 
-//信号处理函数
+//信号处理函数：仅仅只通过管道发送信号值，不处理信号对应的逻辑，缩短异步执行所需要花费的时间，减少对主程序的影响
 void Utils::signal_handler(int signal)
 {
     //为保证函数的可重入性，保留原有errno
+    //可重入性：表示中断后再次进入该函数，环境变量与之前相同，不会丢失数据
     int save_errno = errno;
     int msg = signal;
+
+    //将信号从管道写端写入，传输字符类型，而非整形
     send(u_pipefd[1], (char*)&msg, 1, 0);
+
+    //将原本的errno赋值为当前的errno
     errno = save_errno;
 }
 
-//设置信号函数
+//设置信号函数：仅仅关注SIGTERM和SIGALRM两个信号
 void Utils::addsignal(int signal, void(handler)(int), bool restart)
 {
+    //创建sigaction结构体变量
     struct sigaction sa;
     memset(&sa, '\0', sizeof(sa));
+
+    //信号处理函数中仅仅发送信号值，不做对应逻辑处理
     sa.sa_handler = handler;
     if (restart) 
     {
         sa.sa_flags |= SA_RESTART;
     }
-    sigfillset(&sa.sa_mask);
+    sigfillset(&sa.sa_mask);    //用于将参数set信号集初始化，然后将所有的信号加入到此集合中
+
+    /*
+      int sigaction(int signum, const struct sigaction* act, struct sigaction* oldact);
+      signum:操作的信号
+      act:对信号设置新的处理方式
+      oldact:信号原本的处理方式
+      @return:0.successful -1.error happpen
+     */
     assert(sigaction(signal, &sa, NULL) != -1);
 }
 
@@ -214,7 +232,8 @@ void Utils::addsignal(int signal, void(handler)(int), bool restart)
 void Utils::timer_handler()
 {
     m_timer_lst.tick();
-    alarm(m_TIMESLOT);
+    //设置信号传送闹钟，用来设置信号SIGALRM在经过m_TIMESLOT秒后发送给目前的进程，如果未设置信号的处理函数，则alarm()默认处理终止进程
+    alarm(m_TIMESLOT); 
 }
 
 void Utils::show_error(int connfd, const char* info)
