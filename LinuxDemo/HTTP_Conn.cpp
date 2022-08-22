6d935bd23898198a98e01c1b840824cae5ed6d88
SongX <1647478489@qq.com>
2022-08-22 16:35:40 +0800

    添加HTTP_Conn类事件处理部分代码实现。

--- LinuxDemo/HTTP_Conn.cpp
+++ LinuxDemo/HTTP_Conn.cpp
@@ -1,1 +1,104 @@
+#include <mysql/mysql.h>
+#include <fstream>
+
 #include "HTTP_Conn.h"
+
+//定义http响应状态
+const char* ok_200_title = "OK";
+const char* error_400_title = "Bad Request";
+const char* error_400_form = "Your request has bad syntax or is inherently impossible to staisfy.\n";
+const char* error_403_title = "Forbidden";
+const char* error_403_form = "You don't have permission to get file from this server.\n";
+const char* error_404_title = "Not Found";
+const char* error_404_form = "The requested file wasn't found on this server.\n";
+const char* error_500_title = "Internal Error";
+const char* error_500_form = "There was an unusual problem serving the request file.\n";
+
+locker m_lock;
+map<string, string>users;
+
+//初始化数据库连接
+void http_conn::initmysql_result(connection_pool* connPool)
+{
+	//从连接池中取出连接
+	MYSQL* mysql = NULL;
+	connection_pool_RAII mysqlconn(&mysql, connPool);
+
+	//检索数据库表
+	if (mysql_query(mysql, "SELECT username,pwd from user"))
+	{
+		LOG_ERROR("SELECT ERROR:%s\n", mysql_error(mysql));
+	}
+	//从数据库表中检索出完整的结果集
+	MYSQL_RES* result = mysql_store_result(mysql);
+	//返回结果集中的列数
+	int num_fields = mysql_num_fields(result);
+	//从结果集中获取下一行，将对应的用户名和密码存入map中
+	while (MYSQL_ROW row=mysql_fetch_row(result))
+	{
+		string temp1(row[0]);
+		string temp2(row[1]);
+		users[temp1] = temp2;
+	}
+}
+
+//对文件描述符设置非阻塞
+int setnonblocking(int fd)
+{
+	int old_option = fcntl(fd, F_GETFL);
+	int new_option = old_option | O_NONBLOCK;
+	fcntl(fd, F_SETFL, new_option);
+	return old_option;
+}
+
+//将内核事件表注册读事件，ET模式，选择开启
+void addfd(int epollfd, int fd, bool one_shot, int TRIGMode)
+{
+	epoll_event event;
+	event.data.fd = fd;
+
+	if (1 == TRIGMode)
+	{
+		event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
+	}
+	else
+	{
+		event.events = EPOLLIN | EPOLLRDHUP;
+	}
+
+	if (one_shot)
+	{
+		event.events |= EPOLLONESHOT;
+	}
+	epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+	setnonblocking(fd);
+}
+
+//从内核事件表删除描述符
+void removefd(int epollfd, int fd)
+{
+	epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, 0);
+	close(fd);
+}
+
+//将事件重置为EPOLLONESHOT
+void modfd(int epollfd, int fd, int ev, int TRIGMode)
+{
+	epoll_event event;
+	event.data.fd = fd;
+
+	if (1 == TRIGMode)
+	{
+		event.events = ev | EPOLLET | EPOLLONESHOT | EPOLLRDHUP;
+	}
+	else
+	{
+		event.events = ev | EPOLLONESHOT | EPOLLRDHUP;
+	}
+	epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event);
+}
+
+int http_conn::m_user_count = 0;
+int http_conn::m_epollfd = -1;
+
+
