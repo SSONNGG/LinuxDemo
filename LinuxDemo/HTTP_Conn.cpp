f078503c770d8dfb9828e0fac982652ca3012bbf
SongX <1647478489@qq.com>
2022-08-24 16:56:02 +0800

    添加HTTP_Conn类接口的部分实现，包括request和response请求函数

--- LinuxDemo/HTTP_Conn.cpp
+++ LinuxDemo/HTTP_Conn.cpp
@@ -306,3 +306,288 @@ http_conn::HTTP_CODE http_conn::parse_request_line(char* text)
 	m_check_state = CHECK_STATE_HEADER;
 	return NO_REQUEST;
 }
+
+//解析http请求头
+http_conn::HTTP_CODE http_conn::parse_headers(char* text)
+{
+	if (text[0] == '\0')
+	{
+		if (m_content_length != 0)
+		{
+			m_check_state = CHECK_STATE_CONTENT;
+			return NO_REQUEST;
+		}
+		return GET_REQUEST;
+	}
+	else if (strncasecmp(text, "Connection", 11) == 0)
+	{
+		text += 11;
+		text += strspn(text, " \t");
+		if (strcasecmp(text, "keep-alive") == 0)
+		{
+			m_linger = true;
+		}
+	}
+	else if (strncasecmp(text, "Content-length", 15) == 0)
+	{
+		text += 15;
+		text += strspn(text, " \t");
+		m_content_length = atol(text);
+	}
+	else if (strncasecmp(text, "Host:", 5))
+	{
+		text += 5;
+		text += strspn(text, " \t");
+		m_host = text;
+	}
+	else
+	{
+		LOG_INFO("UNKNOW HEADER!PLEASE RETRY:%s", text);
+	}
+	return NO_REQUEST;
+
+}
+
+//判断http请求是否被完整读入
+http_conn::HTTP_CODE http_conn::parse_content(char* text)
+{
+	if (m_read_idx >= (m_content_length + m_checked_idx))
+	{
+		text[m_content_length] = '\0';
+		//POST请求中最后为输入的用户名和密码
+		m_string = text;
+		return GET_REQUEST;
+	}
+	return NO_REQUEST;
+}
+
+http_conn::HTTP_CODE http_conn::do_request()
+{
+	strcpy(m_real_file, doc_root);
+	int len = strlen(doc_root);
+	const char* p = strrchr(m_url, '/');
+
+	//处理CGI
+	if (cgi == 1 && (*(p + 1) == '2' || *(p + 1) == '3'))
+	{
+		//根据标志判断是登录检测还是注册检测
+		char flag = m_url[1];
+		char* m_url_real = (char*)malloc(sizeof(char) * 200);
+		strcpy(m_url_real, "/");
+		strcpy(m_url_real, m_url + 2);
+		strncpy(m_real_file + len, m_url_real, FILENAME_LEN - len - 1);
+		free(m_url_real);
+
+		//提取用户名和密码
+		char name[100], password[100];
+		int i;
+		for (i = 5; m_string[i] != '&'; ++i)
+		{
+			name[i - 5] = m_string[i];
+		}
+		name[i - 5] = '\0';
+
+		int j = 0;
+		for (i = i + 10; m_string[i] != '\0'; ++i, ++j) {
+			password[j] = m_string[i];
+		}
+		password[j] = '\0';
+
+		if (*(p + 1) == '3')	//注册，先检测数据库中是否有重名的用户
+		{
+			char* sql_insert = (char*)malloc(sizeof(char) * 200);
+			strcpy(sql_insert, "INSERT INTO user(username,pwd) Values(");
+			strcat(sql_insert, "'");
+			strcat(sql_insert, name);
+			strcat(sql_insert, "' , '");
+			strcat(sql_insert, password);
+			strcat(sql_insert, "')");
+
+			if (users.find(name) == users.end())
+			{
+				m_lock.lock();
+				int res = mysql_query(mysql, sql_insert);
+				users.insert(pair<string, string>(name, password));
+				m_lock.unlock();
+
+				if (!res)
+				{
+					strcpy(m_url, "/log.html");
+				}
+				else
+				{
+					strcpy(m_url, "/registerError.html");
+				}
+			}
+			else
+			{
+				strcpy(m_url, "/registerError.html");
+			}
+		}
+		else if (*(p + 1) == '2')	//如果是登录，直接判断
+		{
+			if (users.find(name) != users.end() && users[name] == password)
+			{
+				strcpy(m_url, "/welcome.html");
+			}
+			else
+			{
+				strcpy(m_url, "/logError.html");
+			}
+		}
+	}
+	if (*(p + 1) == '0')
+	{
+		char* m_url_real = (char*)malloc(sizeof(char) * 200);
+		strcpy(m_url_real, "/register.html");
+		strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+		free(m_url_real);
+	}
+	else if (*(p + 1) == '1')
+	{
+		char* m_url_real = (char*)malloc(sizeof(char) * 200);
+		strcpy(m_url_real, "/log.html");
+		strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+		free(m_url_real);
+	}
+	else if (*(p + 1) == '5')
+	{
+		char* m_url_real = (char*)malloc(sizeof(char) * 200);
+		strcpy(m_url_real, "/picture.html");
+		strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+		free(m_url_real);
+	}
+	else if (*(p + 1) == '6')
+	{
+		char* m_url_real = (char*)malloc(sizeof(char) * 200);
+		strcpy(m_url_real, "/video.html");
+		strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+		free(m_url_real);
+	}
+	else if (*(p + 1) == '7')
+	{
+		char* m_url_real = (char*)malloc(sizeof(char) * 200);
+		strcpy(m_url_real, "/fans.html");
+		strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+		free(m_url_real);
+	}
+	else
+	{
+		strncpy(m_real_file + len, m_url, FILENAME_LEN - len - 1);
+	}
+	
+	if (stat(m_real_file, &m_file_stat) < 0)
+	{
+		return NO_RESOURCE;
+	}
+
+	if (!(m_file_stat.st_mode & S_IROTH))
+	{
+		return FORBIDDEN_REQUEST;
+	}
+
+	if (S_ISDIR(m_file_stat.st_mode))
+	{
+		return BAD_REQUEST;
+	}
+
+	int fd = open(m_real_file, O_RDONLY);
+	m_file_address = (char*)mmap(0, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	close(fd);
+	return FILE_REQUEST;
+}
+
+
+void http_conn::unmap()
+{
+	if (m_file_address)
+	{
+		munmap(m_file_address, m_file_stat.st_size);
+		m_file_address = 0;
+	}
+}
+
+bool http_conn::write()
+{
+	int temp = 0;
+
+	if (bytes_to_send == 0)
+	{
+		modfd(m_epollfd, m_sockfd, EPOLLIN, m_TRIGMODE);
+		init();
+		return true;
+	}
+
+	while (1)
+	{
+		temp = writev(m_sockfd, m_iv, m_iv_count);
+
+		if (temp < 0)
+		{
+			if (errno == EAGAIN)
+			{
+				modfd(m_epollfd, m_sockfd, EPOLLOUT, m_TRIGMODE);
+				return true;
+			}
+			unmap();
+			return false;
+		}
+
+		bytes_have_send += temp;
+		bytes_to_send -= temp;
+		if (bytes_have_send >= m_iv[0].iov_len)
+		{
+			m_iv[0].iov_len = 0;
+			m_iv[1].iov_base = m_file_address + (bytes_have_send - m_write_idx);
+			m_iv[1].iov_len = bytes_to_send;
+		}
+		else
+		{
+			m_iv[0].iov_base = m_write_buf + bytes_have_send;
+			m_iv[0].iov_len = m_iv[0].iov_len - bytes_have_send;
+		}
+
+		if (bytes_to_send <= 0)
+		{
+			unmap();
+			modfd(m_epollfd, m_sockfd, EPOLLIN, m_TRIGMODE);
+
+			if (m_linger)
+			{
+				init();
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
+
+bool http_conn::add_response(const char* format, ...)
+{
+	if (m_write_idx >= WRITE_BUFFER_SIZE)
+	{
+		return false;
+	}
+	va_list arg_list;
+	va_start(arg_list, format);
+	int len = vsnprintf(m_write_buf + m_write_idx, WRITE_BUFFER_SIZE - 1 - m_write_idx, format, arg_list);
+	if (len >= (WRITE_BUFFER_SIZE - 1 - m_write_idx))
+	{
+		va_end(arg_list);
+		return false;
+	}
+	m_write_idx += len;
+	va_end(arg_list);
+
+	LOG_INFO("request:%s", m_write_buf);
+
+	return true;
+}
